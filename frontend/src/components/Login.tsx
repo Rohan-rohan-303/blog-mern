7b0a9cd7b51ee82fd86e6a722ad236fec54d7fd6
Rohan-rohan-102 <rohandhakal303@gmail.com>
2026-06-30 08:54:24 +0545

    ...

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate, useLocation} from "react-router-dom";
+import axios from "axios";
+import config from "../config.ts";
+import {Button} from "./ui/button";
+import {Input} from "./ui/input";
+import {Label} from "./ui/label";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "./ui/card";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+interface LocationState {
+  isSignupButtonClicked: boolean;
 }
 
-export default Login
+interface ApiResponse {
+  user: {
+  _id: string;
+  name?: string;
+  email: string;
+  };
+}
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const {isSignupButtonClicked} = (location.state as LocationState) || {};
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [isSignUp, setIsSignUp] = useState<boolean>(isSignupButtonClicked || false);
+  
+
+
+
+}
+
+
